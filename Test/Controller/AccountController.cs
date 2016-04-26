65a21394316363c7879ef6e045f09c433051c02c
xiaosanri <452217943@qq.com>
2016-04-26 16:43:15 +0800

    2016-04-26 company

---
+++ Test/Controller/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controller
+{
+    public class AccountController : System.Web.Mvc.Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            int n = 12;
+
+
+            Math.Sqrt(12);
+
+            Math.Pow(n, 2);
+
+            for (int i = (int)Math.Pow(n, 2); i > 1; i--)
+            {
+
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Test/Controller/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controller
+{
+    public class TestController : System.Web.Mvc.Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SecondView()
+        {
+            return View();
+        }
+
+        public ActionResult Hotel()
+        {
+            return View();
+        }
+    }
+}
